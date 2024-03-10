d933593c763682a13d0499987042bad35e4ea133
shengkai23444 <shengkai23444@gmail.com>
2024-03-11 00:43:12 +0800

    add user, store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20240310100736) do
+ActiveRecord::Schema.define(version: 20240310104920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,23 @@ ActiveRecord::Schema.define(version: 20240310100736) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "vote_logs_count"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "title"
+    t.string   "tel"
+    t.string   "address"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "tel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "vote_logs", force: :cascade do |t|
