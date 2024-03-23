2d87fb2af2dde94eaa3ddefacaccb9d2a5323808
shengkai23444 <shengkai23444@gmail.com>
2024-03-23 09:30:09 +0800

    I don't know...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+require 'digest'
+
 class User < ActiveRecord::Base
     has_one :store
+    
+    validate :name_validator
+
+    before_create :encrypt_email
+
+    private
+    def name_validator
+        unless name.starts_with? 'Ruby'
+            errors[:name] << "必須是 Ruby 開頭喔！"
+        end
+    end
+
+    def encrypt_email
+        self.email = Digest::MD5.hexdigest(email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get "bmi", to: "bmi#index"
 
   get "/hello_world", to: "pages#hello"
